a15b4a6a7f48297f5373c9f14e1571ac617e270a
Scott Offen <3513626+scottoffen@users.noreply.github.com>
2021-10-01 17:28:53 -0600

    Updates LuckyNumber instructions

--- src/lucky-number/LuckyNumber/Program.cs
+++ src/lucky-number/LuckyNumber/Program.cs
@@ -13,15 +13,10 @@ namespace LuckyNumber
                     - "is lucky" if the number is lucky
                     - "is not lucky" if the number is not lucky
 
-            Have the program run in an external console.
-
             A number is considered lucky if:
                 - it is at least six characters long
                 - it contains *only* numeric characters (0-9)
                 - the sum of the first half of the digits equals the sum of the second half of the digits.
-
-            Some sample code is provided as a possible starting point, feel
-            free to use it or remove it.
         */
 
         private static List<string> numbers = new List<string>
@@ -47,8 +42,7 @@ namespace LuckyNumber
             Console.ReadLine();
         }
 
-        // Feel free to change the input parameter for your implementation
-        public static bool IsLucky(object number)
+        public static bool IsLucky(string number)
         {
             return false;
         }
